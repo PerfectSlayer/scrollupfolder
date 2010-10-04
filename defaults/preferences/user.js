259ec5ebfe5da67b65463d0833abcc17d86fc41e
Perfect Slayer <PerfectSlayer@users.noreply.github.com>
2010-10-04 16:56:16 +0000

    Adding urlbar icon to display panel

--- defaults/preferences/user.js
+++ defaults/preferences/user.js
@@ -1,3 +1,4 @@
+pref("extensions.scrollupfolder.showButton", true);
 pref("extensions.scrollupfolder.controlMode", 3);
 pref("extensions.scrollupfolder.badUriAction", 0);
 pref("extensions.scrollupfolder.parseGetVars", true);
