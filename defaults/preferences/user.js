30d690c624197dabb2d4f1755157363d9d4d137b
Perfect Slayer <PerfectSlayer@users.noreply.github.com>
2012-01-15 11:09:03 +0000

    Add preference to invert scroll axis  - Fix issue 20  - Only english and french locales (more will be with babelzilla) Upgrade max version to firefox 12.0a1

--- defaults/preferences/user.js
+++ defaults/preferences/user.js
@@ -1,5 +1,6 @@
 pref("extensions.scrollupfolder.showButton", true);
 pref("extensions.scrollupfolder.controlMode", 3);
 pref("extensions.scrollupfolder.badUriAction", 0);
+pref("extensions.scrollupfolder.invertScroll", false);
 pref("extensions.scrollupfolder.parseGetVars", true);
 pref("extensions.scrollupfolder.version", "uninstalled");
