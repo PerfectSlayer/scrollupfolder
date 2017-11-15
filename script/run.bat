245d9496597702070212d76a3a3e13cc7bf7ea4c
PerfectSlayer <bruce.bujon@gmail.com>
2017-11-15 21:14:01 +0100

    Improve run script to display start-up page

--- script/run.bat
+++ script/run.bat
@@ -1,1 +1,1 @@
-web-ext run --firefox="C:\Program Files\Firefox Developer Edition\firefox.exe" --firefox-profile %CD%\script\profile --keep-profile-changes
+web-ext run --firefox="C:\Program Files\Firefox Developer Edition\firefox.exe" --firefox-profile %CD%\script\profile --keep-profile-changes --start-url https://github.com/PerfectSlayer/scrollupfolder
